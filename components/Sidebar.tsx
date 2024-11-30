e7fd7eb960c400e7d133b22d6ebb2f78dde0e4d3
mitibirru <357miteshbirru@gmail.com>
2024-11-30 17:26:52 +0530

    initial setup

---
+++ components/Sidebar.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { CoinsIcon, HomeIcon, Layers2Icon, ShieldCheckIcon } from 'lucide-react';
+import React from 'react';
+
+const routes = [
+	{
+		href: '',
+		label: 'Home',
+		icon: HomeIcon
+	},
+	{
+		href: 'workflows',
+		label: 'Workflows',
+		icon: Layers2Icon
+	},
+	{
+		href: 'credentials',
+		label: 'Credentials',
+		icon: ShieldCheckIcon
+	},
+	{
+		href: 'billing',
+		label: 'Billing',
+		icon: CoinsIcon
+	}
+];
+
+function DesktopSidebar() {
+	return (
+		<div className="hidden relative md:block min-w-[280px] max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 dark:text-foreground text-muted-foreground border-r-2 border-separate">
+			DesktopSidebar
+		</div>
+	);
+}
+
+export default DesktopSidebar;
